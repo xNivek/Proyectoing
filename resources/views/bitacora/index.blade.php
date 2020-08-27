b566ecc1d838a2b2865ad350245be3ee81a2830b
JotapeValdesCondori <juan.valdes@alumnos.ucn.cl>
2020-08-27 18:00:01 -0400

    listo

--- resources/views/bitacora/index.blade.php
+++ resources/views/bitacora/index.blade.php
@@ -40,8 +40,8 @@
                                             <a href="{{ route('bitacora.show', $bitacora->id) }}" class="btn btn-sm btn-primary">Ver</a>
                                             <!--<a href="{{ route('indice', $bitacora->id) }}" class="btn btn-sm btn-primary">Avance</a>--> 
                                             <a href="{{ route('bitacora.edit', $bitacora->id) }}" class="btn btn-sm btn-primary">Editar</a>
-                                            <a href="" class="btn btn-sm btn-danger">Finalizar por No Continuidad</a>
-                                            <a href="" class="btn btn-sm btn-danger">Finalizar por Aprobacion </a>
+                                            <!--<a href="" class="btn btn-sm btn-danger">Finalizar por No Continuidad</a>
+                                            <a href="" class="btn btn-sm btn-danger">Finalizar por Aprobacion </a>-->
                                             
 
                                             
